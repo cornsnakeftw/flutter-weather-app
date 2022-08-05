fbbdef30e0fc9397ca0030b68db93eadc8342260
cornsnakeftw <marianne@besquare.com.my>
2022-08-05 17:55:48 +0800

    able to show icon based on first description

--- lib/widget/weather_information.dart
+++ lib/widget/weather_information.dart
@@ -12,7 +12,7 @@ class WeatherInformation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Card(
           shape: RoundedRectangleBorder(
@@ -20,24 +20,49 @@ class WeatherInformation extends StatelessWidget {
               borderRadius: BorderRadius.circular(10)),
           child: Container(
             padding: EdgeInsets.all(16),
-            height: 300,
-            width: 300,
+            height: 500,
+            width: 400,
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Icon(Icons.wb_sunny, color: Colors.yellow, size: 40),
-                const SizedBox(height: 10),
+                // Icon(Icons.wb_sunny, color: Colors.yellow, size: 40),
+                // const SizedBox(height: 10),
+                Image.network(
+                    'http://openweathermap.org/img/wn/${weatherModel.weather.first.icon}@2x.png'),
                 Text('${weatherModel.main.temp} °C',
                     style:
                         TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
                 const SizedBox(
                   height: 10,
                 ),
-                Text('${weatherModel.name} ', style: TextStyle(fontSize: 20)),
+                Text('${weatherModel.name} ',
+                    style:
+                        TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
                 const SizedBox(
                   height: 10,
                 ),
-                Text('${weatherModel.weather.first.description} '),
+                Text('First Desc: ${weatherModel.weather.first.description} '),
+                const SizedBox(
+                  height: 10,
+                ),
+                //-------------------------------------------------------------
+                Text('Humidity : ${weatherModel.main.humidity} '),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text('Min Temp: ${weatherModel.main.tempMin} '),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text('Max Temp: ${weatherModel.main.tempMax} '),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text('Pressure: ${weatherModel.main.pressure} '),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text('Feels like: ${weatherModel.main.feelsLike} '),
                 const SizedBox(
                   height: 10,
                 ),
